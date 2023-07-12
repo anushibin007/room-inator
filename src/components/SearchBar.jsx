a3bff7ad1c9e1ce2116a3f87454ac51002855f0f
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2023-07-12 15:51:08 +0530

    lunr JS implemented

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,20 +2,90 @@
 import { useEffect, useState } from "react";
 import Form from "react-bootstrap/Form";
 import Rooms from "../data/RoomsDB";
+import lunr from "lunr";
 
 function SearchBar({ rooms, setRooms }) {
 	const [searchInput, setSearchInput] = useState("");
+
+	/**
+	 *
+	 * The lunr index is built locally when
+	 * the app renders for the first time in
+	 * the UI.
+	 *
+	 * TODO: change this to happen at build
+	 * and just bundle the index with the
+	 * app artifact. Read more here:
+	 * https://lunrjs.com/guides/index_prebuilding.html
+	 *
+	 */
+	const lunrIndex = lunr(function () {
+		// id is the primary key
+		this.ref("id");
+
+		this.field("c", { boost: 10 }); // index country
+		this.field("l", { boost: 50 }); // index location
+		this.field("b", { boost: 90 }); // index building
+		this.field("f", { boost: 60 }); // index floor
+		this.field("n", { boost: 100 }); // index name
+
+		Rooms.forEach(function (aRoom) {
+			this.add(aRoom);
+		}, this);
+	});
 
 	const handleSearchInputChange = (e) => {
 		setSearchInput(e.target.value);
 	};
 
 	useEffect(() => {
-		const searchedRooms = Rooms.filter((aRoom) => {
-			return aRoom.n.toLowerCase().includes(searchInput.toLowerCase());
-		});
-		setRooms(searchedRooms);
+		setRooms(filterData(searchInput));
 	}, [searchInput]);
+
+	/**
+	 * This is the old simple JS
+	 * method that uses plain JS
+	 * functions to perform the search.
+	 *
+	 * @deprecated use filterData instead
+	 * @param {*} aSearchInput
+	 * @returns
+	 */
+	const oldFilterData = (aSearchInput) => {
+		const searchedRooms = Rooms.filter((aRoom) => {
+			return aRoom.n.toLowerCase().includes(aSearchInput.toLowerCase());
+		});
+		return searchedRooms;
+	};
+
+	/**
+	 * A new filter method that uses the
+	 * lunr index that we build in the
+	 * beginning of the app load.
+	 *
+	 * @param {*} aSearchInput
+	 * @returns
+	 */
+	const filterData = (aSearchInput) => {
+		// Ignore empty string searches
+		if (!aSearchInput || aSearchInput.trim().length <= 0) {
+			return Rooms;
+		}
+
+		// TODO: Search partial words too
+		const indexSearchResult = lunrIndex.search(`${aSearchInput}`);
+		console.log(indexSearchResult);
+		var searchedRooms = [];
+		indexSearchResult.forEach((anIndexResult) => {
+			searchedRooms.push(
+				Rooms.filter((aRoom) => {
+					return aRoom.id == anIndexResult.ref;
+				})[0]
+			);
+		});
+		console.log(searchedRooms);
+		return searchedRooms;
+	};
 
 	return (
 		<>
