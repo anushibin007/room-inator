8e3b1434cd0d22276021c6da7d699e5833f41501
Anu Shibin Joseph Raj <anushibin007@gmail.com>
2023-08-16 23:21:02 +0530

    Enable Dark Mode by default

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import React, { useState } from "react";
 // DB
 import Rooms from "./data/RoomsDB";
 // MUI
+import { CssBaseline } from "@mui/joy";
 import Grid from "@mui/joy/Grid";
+import { CssVarsProvider, extendTheme } from "@mui/joy/styles";
 // Other internal Components
 import QuickFilters from "./components/QuickFilters";
 import Footer from "./components/Footer";
@@ -15,30 +17,39 @@ function App() {
 	const [rooms, setRooms] = useState(Rooms);
 	const [darkMode, setDarkMode] = useState(false);
 
+	const theme = extendTheme({ cssVarPrefix: "dark" });
+
 	const toggleDarkMode = () => {
 		setDarkMode(!darkMode);
 	};
 	return (
 		<>
-			<Grid container paddingX={3}>
-				<Grid xs={12}>
-					<Header
-						rooms={rooms}
-						setRooms={setRooms}
-						darkMode={darkMode}
-						toggleDarkMode={toggleDarkMode}
-					/>
+			<CssVarsProvider
+				theme={theme}
+				modeStorageKey="demo_identify-system-mode"
+				disableNestedContext
+			>
+				<CssBaseline />
+				<Grid container paddingX={3}>
+					<Grid xs={12}>
+						<Header
+							rooms={rooms}
+							setRooms={setRooms}
+							darkMode={darkMode}
+							toggleDarkMode={toggleDarkMode}
+						/>
+					</Grid>
+					{
+						//<QuickFilters rooms={rooms} setRooms={setRooms} />
+					}
+					<Grid xs={12}>
+						<SearchResults rooms={rooms} darkMode={darkMode} />
+					</Grid>
+					{
+						//<Footer />
+					}
 				</Grid>
-				{
-					//<QuickFilters rooms={rooms} setRooms={setRooms} />
-				}
-				<Grid xs={12}>
-					<SearchResults rooms={rooms} darkMode={darkMode} />
-				</Grid>
-				{
-					//<Footer />
-				}
-			</Grid>
+			</CssVarsProvider>
 		</>
 	);
 }
