ec07b47cbc28b4626851b7681d75e81f65318cf0
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2023-10-27 13:11:31 +0530

    ⚰ Disabled CICD test for now

--- src/tests/RoomsDB.test.js
+++ src/tests/RoomsDB.test.js
@@ -1,38 +1,8 @@
-import { describe, it, expect, test } from "vitest";
-import Rooms from "../data/RoomsDB";
+import { describe } from "vitest";
 
 describe("Validate RoomsDB", () => {
-	const allRooms = Rooms;
-
-	allRooms.forEach((room) => {
-		it(`checking undefined for Room ID ${room.id}`, () => {
-			// Check RoomsDB.js to
-			// understand the meaning of each key
-			expect(room.id).not.toBeUndefined();
-			expect(room.c).not.toBeUndefined();
-			expect(room.l).not.toBeUndefined();
-			expect(room.b).not.toBeUndefined();
-			expect(room.f).not.toBeUndefined();
-			expect(room.n).not.toBeUndefined();
-			expect(room.s).not.toBeUndefined();
-			expect(room.i).not.toBeUndefined();
-			expect(room.wb).not.toBeUndefined();
-			expect(room.pr).not.toBeUndefined();
-		});
-	});
-
-	allRooms.forEach((room) => {
-		it(`checking correct datatype for Room ID ${room.id}`, () => {
-			expect(typeof room.id).toBe("string");
-			expect(typeof room.c).toBe("string");
-			expect(typeof room.l).toBe("string");
-			expect(typeof room.b).toBe("string");
-			expect(typeof room.f).toBe("number");
-			expect(typeof room.n).toBe("string");
-			expect(typeof room.s).toBe("number");
-			expect(typeof room.i).toBe("object");
-			expect(typeof room.wb).toBe("boolean");
-			expect(typeof room.pr).toBe("boolean");
-		});
-	});
+	// No JSON validation tests anymore
+	// because the JSON is not stored
+	// in the repo. It has to be provided
+	// by the user in the public folder
 });
