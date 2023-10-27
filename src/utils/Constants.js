b1f8a146bf56dc63a0c3ab2519307376151b7c4c
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2023-10-27 19:46:39 +0530

    🚚 BASE_PATH → VITE_BASE_PATH

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,3 +1,3 @@
-const BASE_PATH = import.meta.env.BASE_PATH || "/room-inator/";
+const BASE_PATH = import.meta.env.VITE_BASE_PATH || "/room-inator/";
 
 export default { BASE_PATH };
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-const BASE_PATH = process.env.BASE_PATH || "/room-inator/";
+const BASE_PATH = process.env.VITE_BASE_PATH || "/room-inator/";
 
 export default defineConfig({
 	plugins: [react()],
