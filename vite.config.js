0848535e88fdf9fc1f85a11e0be49b45386f6c1c
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2023-10-27 17:09:10 +0530

    🔧 Allow modifying BASE_PATH via ENV

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+const BASE_PATH = process.env.BASE_PATH || "/room-inator/";
+
 export default defineConfig({
 	plugins: [react()],
-	base: "/room-inator/",
+	base: BASE_PATH,
 });
