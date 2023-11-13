74132f70829855cc820703a07143e7526fe7aa65
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2023-11-14 00:41:26 +0530

    🎨 Navbar should refresh page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import Grid from "@mui/joy/Grid";
 
 // Bootstrap
 import SearchBar from "./SearchBar";
+import Constants from "../utils/Constants";
 import { Typography, IconButton } from "@mui/joy";
 import MeetingRoomIcon from "@mui/icons-material/MeetingRoom";
 import FilterAltIcon from "@mui/icons-material/FilterAlt";
@@ -17,9 +18,11 @@ function Header({ rooms, setRooms, darkMode, toggleDarkMode }) {
 		<>
 			<Grid container xs={12} alignItems="center" justifyContent="space-between" padding={2}>
 				<Grid xs={8}>
-					<Typography level="title-lg" startDecorator={<MeetingRoomIcon />}>
-						Room-Inator
-					</Typography>
+					<a href={Constants.BASE_PATH}>
+						<Typography level="title-lg" startDecorator={<MeetingRoomIcon />}>
+							Room-Inator
+						</Typography>
+					</a>
 				</Grid>
 				<Grid xs={1}>
 					<IconButton onClick={toggleModal}>
