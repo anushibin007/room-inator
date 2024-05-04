51544858d2bbe2b72b594fd782c0d118089fc52c
Anu Shibin Joseph Raj <anushibin007@gmail.com>
2024-05-04 18:56:21 +0530

    🎨 Make the image reactive

--- src/components/RoomDetails.jsx
+++ src/components/RoomDetails.jsx
@@ -43,136 +43,148 @@ function RoomDetails(props) {
 										<img src={`${anImageSrc}?random=${index}`} />
 									</Grid>
 								))*/}
-								<Grid container xs={12} alignItems="center" justifyContent="center">
+								<Grid
+									container
+									xs={12}
+									sx={{ padding: 3 }}
+									alignItems="center"
+									justifyContent="center"
+								>
 									<Grid>
 										<img
 											src={`${room.i ? room.i[0] : ""}`}
-											style={{ maxHeight: "500px", maxWidth: "500px" }}
+											style={{ maxWidth: "100%" }}
 										/>
 									</Grid>
 								</Grid>
 							</Grid>
 							<Grid container xs={12} md={6} alignItems="center">
-								<Table>
-									<tbody>
-										{room.c && (
-											<tr>
-												<td>
-													<Typography level="body-lg">Country</Typography>
-												</td>
-												<td>
-													<Typography level="body-md">
-														{room.c}
-													</Typography>
-												</td>
-											</tr>
-										)}
-										{room.l && (
-											<tr>
-												<td>
-													<Typography level="body-lg">
-														Location
-													</Typography>
-												</td>
-												<td>
-													<Typography level="body-md">
-														{room.l}
-													</Typography>
-												</td>
-											</tr>
-										)}
-										{room.b && (
-											<tr>
-												<td>
-													<Typography level="body-lg">
-														Building
-													</Typography>
-												</td>
-												<td>
-													<Typography level="body-md">
-														{room.b}
-													</Typography>
-												</td>
-											</tr>
-										)}
-										{room.f && (
-											<tr>
-												<td>
-													<Typography level="body-lg">Floor</Typography>
-												</td>
-												<td>
-													<Typography level="body-md">
-														{room.f}
-													</Typography>
-												</td>
-											</tr>
-										)}
-										{room.s && (
-											<tr>
-												<td>
-													<Typography level="body-lg">
-														Seating Capacity
-													</Typography>
-												</td>
-												<td>
-													<Typography level="body-md">
-														{room.s}
-													</Typography>
-												</td>
-											</tr>
-										)}
-										{room.wb != undefined && (
-											<tr>
-												<td>
-													<Typography level="body-lg">
-														White Board
-													</Typography>
-												</td>
-												<td>
-													<Typography level="body-md">
-														{room.wb ? "✅" : "❌"}
-													</Typography>
-												</td>
-											</tr>
-										)}
-										{room.pr != undefined && (
-											<tr>
-												<td>
-													<Typography level="body-lg">
-														Projector
-													</Typography>
-												</td>
-												<td>
-													<Typography level="body-md">
-														{room.pr ? "✅" : "❌"}
-													</Typography>
-												</td>
-											</tr>
-										)}
-										{room.di && (
-											<>
+								<Grid xs={12} sx={{ padding: 3 }}>
+									<Table>
+										<tbody>
+											{room.c && (
 												<tr>
-													<td colSpan={2}>
+													<td>
 														<Typography level="body-lg">
-															Directions
+															Country
+														</Typography>
+													</td>
+													<td>
+														<Typography level="body-md">
+															{room.c}
 														</Typography>
 													</td>
 												</tr>
+											)}
+											{room.l && (
 												<tr>
-													<td colSpan={2}>
-														<ol>
-															{room.di.map((direction, index) => (
-																<React.Fragment key={index}>
-																	<li>{direction}</li>
-																</React.Fragment>
-															))}
-														</ol>
+													<td>
+														<Typography level="body-lg">
+															Location
+														</Typography>
+													</td>
+													<td>
+														<Typography level="body-md">
+															{room.l}
+														</Typography>
 													</td>
 												</tr>
-											</>
-										)}
-									</tbody>
-								</Table>
+											)}
+											{room.b && (
+												<tr>
+													<td>
+														<Typography level="body-lg">
+															Building
+														</Typography>
+													</td>
+													<td>
+														<Typography level="body-md">
+															{room.b}
+														</Typography>
+													</td>
+												</tr>
+											)}
+											{room.f && (
+												<tr>
+													<td>
+														<Typography level="body-lg">
+															Floor
+														</Typography>
+													</td>
+													<td>
+														<Typography level="body-md">
+															{room.f}
+														</Typography>
+													</td>
+												</tr>
+											)}
+											{room.s && (
+												<tr>
+													<td>
+														<Typography level="body-lg">
+															Seating Capacity
+														</Typography>
+													</td>
+													<td>
+														<Typography level="body-md">
+															{room.s}
+														</Typography>
+													</td>
+												</tr>
+											)}
+											{room.wb != undefined && (
+												<tr>
+													<td>
+														<Typography level="body-lg">
+															White Board
+														</Typography>
+													</td>
+													<td>
+														<Typography level="body-md">
+															{room.wb ? "✅" : "❌"}
+														</Typography>
+													</td>
+												</tr>
+											)}
+											{room.pr != undefined && (
+												<tr>
+													<td>
+														<Typography level="body-lg">
+															Projector
+														</Typography>
+													</td>
+													<td>
+														<Typography level="body-md">
+															{room.pr ? "✅" : "❌"}
+														</Typography>
+													</td>
+												</tr>
+											)}
+											{room.di && (
+												<>
+													<tr>
+														<td colSpan={2}>
+															<Typography level="body-lg">
+																Directions
+															</Typography>
+														</td>
+													</tr>
+													<tr>
+														<td colSpan={2}>
+															<ol>
+																{room.di.map((direction, index) => (
+																	<React.Fragment key={index}>
+																		<li>{direction}</li>
+																	</React.Fragment>
+																))}
+															</ol>
+														</td>
+													</tr>
+												</>
+											)}
+										</tbody>
+									</Table>
+								</Grid>
 							</Grid>
 						</Grid>
 					</>
