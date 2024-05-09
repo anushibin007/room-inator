768939d03ebe8427952c10a7aaff18c60943c2c9
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2024-05-09 21:39:28 +0530

    💩 Removed relation-mapping for now

--- backend/src/main/java/anushibin007/roominator/backend/model/Country.java
+++ backend/src/main/java/anushibin007/roominator/backend/model/Country.java
@@ -1,14 +1,7 @@
 package anushibin007.roominator.backend.model;
 
-import java.util.Set;
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -21,7 +14,4 @@ public class Country {
 	@Id
 	private String id;
 	private String name;
-	@OneToMany(mappedBy = "country", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-	@JsonBackReference
-	private Set<Location> locations;
 }
