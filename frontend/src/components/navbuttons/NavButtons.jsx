e4f7201d7dd855c70a4eda00a24c29d4d00cc792
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2024-05-21 19:55:06 +0530

    💄 Stretch & Center

--- frontend/src/components/navbuttons/NavButtons.jsx
+++ frontend/src/components/navbuttons/NavButtons.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import BackButton from "./BackButton";
 import HomeButton from "./HomeButton";
+import { Grid } from "@mui/joy";
 
 const NavButtons = ({ href }) => {
 	return (
 		<>
-			<BackButton href={href} />
-			<HomeButton />
+			<Grid
+				container
+				direction="row"
+				justifyContent="center"
+				alignItems="center"
+				sx={{ height: "100%" }}
+			>
+				<Grid>
+					<BackButton href={href} />
+				</Grid>
+				<Grid>
+					<HomeButton />
+				</Grid>
+			</Grid>
 		</>
 	);
 };
