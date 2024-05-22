b8655c4d1d1acbd42c840471a3d3ea222a6117df
Divya <divya.gracie@gmail.com>
2024-05-22 21:27:32 +0530

    Backend: Return images in RoomView

--- backend/src/main/java/anushibin007/roominator/backend/models/RoomView.java
+++ backend/src/main/java/anushibin007/roominator/backend/models/RoomView.java
@@ -1,9 +1,12 @@
 package anushibin007.roominator.backend.models;
 
+import java.util.List;
+
 public interface RoomView {
     String getId();
     String getName();
     String getBuildingId();
     Integer getFloor();
     Integer getCapacity();
+    List<String> getImages();
 }
--- backend/src/main/java/anushibin007/roominator/backend/repositories/RoomViewRepository.java
+++ backend/src/main/java/anushibin007/roominator/backend/repositories/RoomViewRepository.java
@@ -14,11 +14,10 @@ public interface RoomViewRepository extends JpaRepository<Room, String> {
     @Query("SELECT r.id as id, r.name as name, r.building.id as buildingId, r.floor as floor, r.capacity as capacity  FROM Room r ORDER BY r.floor, r.name")
     List<RoomView> findAllRoomViews();
 
-
     @Query("SELECT r.id as id, r.name as name, r.building.id as buildingId, r.floor as floor, r.capacity as capacity FROM Room r WHERE r.building.id= :building ORDER BY r.floor, r.name")
     List<RoomView> findRoomViewsByBuildingIdAndRoomName(@Param("building") String building);
 
-    @Query("SELECT r.id as id, r.name as name, r.building.id as buildingId, r.floor as floor, r.capacity as capacity FROM Room r WHERE r.building.id= :building AND UPPER(r.name) like :roomName ORDER BY r.floor, r.name")
+    @Query("SELECT r.id as id, r.name as name, r.building.id as buildingId, r.floor as floor, r.capacity as capacity, r.images as images FROM Room r WHERE r.building.id= :building AND UPPER(r.name) like :roomName ORDER BY r.floor, r.name")
     List<RoomView> findRoomViewsByBuildingIdAndRoomName(@Param("building") String building, @Param("roomName") String roomName);
 
     @Query("SELECT r.id as id, r.name as name, r.building.id as buildingId, r.floor as floor, r.capacity as capacity FROM Room r WHERE r.building.id= :buildingId AND r.capacity= :seatingCapacity ORDER BY r.floor, r.name")
