203615bbb1f5df7f84022cd914d13f6e17d59872
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2024-06-12 11:29:01 +0530

    💄 Middle clickable cards in Grid View

--- frontend/src/components/SearchResultsGridView.jsx
+++ frontend/src/components/SearchResultsGridView.jsx
@@ -9,29 +9,12 @@ import StairsIcon from "@mui/icons-material/Stairs";
 import PeopleIcon from "@mui/icons-material/People";
 
 import NonOverflowingTypography from "./customcomponent/NonOverflowingTypography";
-import { addHashToCurrentPage, buildImageSrcUrl } from "../utils/URLHelper";
+import { buildImageSrcUrl } from "../utils/URLHelper";
 import Constants from "../utils/Constants";
 import SkeletonCardCover from "./customcomponent/SkeletonCardCover";
+import Link from "@mui/joy/Link";
 
 function SearchResultsGridView({ rooms }) {
-	/**
-	 * Function to show the Modal
-	 * @param {*} aRoom The room whose detail needs to be shown in the Modal
-	 * @param {*} e
-	 */
-	const openRoom = (aRoom, e) => {
-		if (typeof aRoom === "object") {
-			// There is a double redirect happening
-			// here. The first one is not sending a
-			// id. Rather, it is sending an object.
-			// I don't know why it is happening
-			// though. We are simply going to ignore
-			// that call.
-			return;
-		}
-		addHashToCurrentPage(`room/${aRoom}`);
-	};
-
 	return (
 		<>
 			<Grid container id="search-results-wrapper" marginTop={1} xs={12}>
@@ -45,14 +28,10 @@ function SearchResultsGridView({ rooms }) {
 						xs={12}
 						md={6}
 						lg={4}
-						onClick={() => {
-							openRoom(room.id);
-						}}
 					>
 						<Grid xs={12}>
 							<Card
 								variant="outlined"
-								onClick={() => openRoom(room)}
 								sx={{ height: "250px", div: { cursor: "pointer" } }}
 							>
 								{room.images && (
@@ -79,33 +58,35 @@ function SearchResultsGridView({ rooms }) {
 									}}
 								/>
 								<CardContent sx={{ justifyContent: "flex-end" }}>
-									<Grid container xs={12}>
-										<Grid xs={12}>
-											<NonOverflowingTypography
-												level="title-lg"
-												textColor="#fff"
-												mb={1}
-											>
-												{room.name}
-											</NonOverflowingTypography>
+									<Link overlay href={`#room/${room.id}`}>
+										<Grid container xs={12}>
+											<Grid xs={12}>
+												<NonOverflowingTypography
+													level="title-lg"
+													textColor="#fff"
+													mb={1}
+												>
+													{room.name}
+												</NonOverflowingTypography>
+											</Grid>
+											<Grid xs={6}>
+												<NonOverflowingTypography
+													startDecorator={<StairsIcon />}
+												>
+													Floor {room.floor}
+												</NonOverflowingTypography>
+											</Grid>
+											<Grid xs={6}>
+												<NonOverflowingTypography
+													startDecorator={<PeopleIcon />}
+													textColor="neutral.300"
+													mb={1}
+												>
+													Capacity {room.capacity}
+												</NonOverflowingTypography>
+											</Grid>
 										</Grid>
-										<Grid xs={6}>
-											<NonOverflowingTypography
-												startDecorator={<StairsIcon />}
-											>
-												Floor {room.floor}
-											</NonOverflowingTypography>
-										</Grid>
-										<Grid xs={6}>
-											<NonOverflowingTypography
-												startDecorator={<PeopleIcon />}
-												textColor="neutral.300"
-												mb={1}
-											>
-												Capacity {room.capacity}
-											</NonOverflowingTypography>
-										</Grid>
-									</Grid>
+									</Link>
 								</CardContent>
 							</Card>
 						</Grid>
