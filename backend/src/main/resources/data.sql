d82214f1beb35738f753c6ff8853b73d3c3507c6
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2024-06-26 14:40:45 +0530

    🤡 Some better mock data

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,27 +1,128 @@
-insert into country (name, id ) values ('India', 'IN');
-insert into country (name, id ) values ('France', 'FN');
-insert into country (name, id ) values ('Germany', 'GM');
-insert into country (name, id ) values ('Italy', 'IT');
-insert into country (name, id ) values ('Japan', 'JP');
-insert into country (name, id ) values ('Korea', 'KR');
-insert into country (name, id ) values ('Mexico', 'MX');
+--- COUNTRIES
+insert into
+    country (name, id)
+values
+    ('India', 'IN');
 
-INSERT INTO LOCATION (COUNTRY_ID, ID, NAME) VALUES ('IN', 'MUM', 'Mumbai');
-INSERT INTO LOCATION (COUNTRY_ID, ID, NAME) VALUES ('IN', 'DL', 'Delhi');
-INSERT INTO LOCATION (COUNTRY_ID, ID, NAME) VALUES ('IN', 'BGLR', 'Bangalore');
-INSERT INTO LOCATION (COUNTRY_ID, ID, NAME) VALUES ('IN', 'CH', 'Chennai');
-INSERT INTO LOCATION (COUNTRY_ID, ID, NAME) VALUES ('FN', 'PAR', 'Paris');
-INSERT INTO LOCATION (COUNTRY_ID, ID, NAME) VALUES ('FN', 'STR', 'Strasbourg');
+insert into
+    country (name, id)
+values
+    ('Dublin', 'DUB');
 
-INSERT INTO BUILDING (LOCATION_ID, ID, NAME) VALUES ('MUM', 'MUM-B1', 'Building 1');
-INSERT INTO BUILDING (LOCATION_ID, ID, NAME) VALUES ('MUM', 'MUM-B2', 'Building 2');
-INSERT INTO BUILDING (LOCATION_ID, ID, NAME) VALUES ('BGLR', 'BGLR-B1', 'Building 3');
-INSERT INTO BUILDING (LOCATION_ID, ID, NAME) VALUES ('BGLR', 'BGLR-B3', 'Building 4');
-INSERT INTO BUILDING (LOCATION_ID, ID, NAME) VALUES ('PAR', 'PAR-B1', 'Paris Building 1');
-INSERT INTO BUILDING (LOCATION_ID, ID, NAME) VALUES ('STR', 'STR-B1', 'Strasbourg Building 1');
+--- LOCATIONS
+INSERT INTO
+    LOCATION (COUNTRY_ID, ID, NAME)
+VALUES
+    ('IN', 'MUM', 'Mumbai');
 
-INSERT INTO ROOM (FLOOR, CAPACITY, BUILDING_ID, ID, NAME, DIRECTIONS, IMAGES, STATIONERY) VALUES ('1', '100', 'MUM-B1', 'BGLR-B1-ID1', 'Room 1', 'Room 1 Directions', ARRAY ['https://picsum.photos/1024/1024', 'https://picsum.photos/1024/1024', 'https://picsum.photos/1024/1024', 'https://picsum.photos/1024/1024'], ARRAY ['whiteboard', 'projector']);
+INSERT INTO
+    LOCATION (COUNTRY_ID, ID, NAME)
+VALUES
+    ('IN', 'BGLR', 'Bangalore');
 
-INSERT INTO ROOM (FLOOR, CAPACITY, BUILDING_ID, ID, NAME, DIRECTIONS, IMAGES, STATIONERY) VALUES ('1', '100', 'MUM-B2', 'BGLR-B2-ID1', 'Room 2', 'Room 2 Directions', ARRAY ['https://picsum.photos/1024/1024', 'https://picsum.photos/1024/1024', 'https://picsum.photos/1024/1024', 'https://picsum.photos/1024/1024'], ARRAY ['whiteboard']);
+INSERT INTO
+    LOCATION (COUNTRY_ID, ID, NAME)
+VALUES
+    ('DUB', 'GC', 'Grand Canal');
 
-INSERT INTO ROOM (FLOOR, CAPACITY, BUILDING_ID, ID, NAME, DIRECTIONS, IMAGES, STATIONERY) VALUES ('15', '100', 'PAR-B1', 'PAR-B1-ID1', 'Paris Building Room 1', 'Paris Building Room 1 Directions', ARRAY ['https://picsum.photos/1024/1024', 'https://picsum.photos/1024/1024', 'https://picsum.photos/1024/1024', 'https://picsum.photos/1024/1024'], ARRAY ['projector']);
+--- BUILDINGS
+INSERT INTO
+    BUILDING (LOCATION_ID, ID, NAME)
+VALUES
+    ('MUM', 'MUM-B1', 'Accenture Primal Tower');
+
+INSERT INTO
+    BUILDING (LOCATION_ID, ID, NAME)
+VALUES
+    ('MUM', 'MUM-B2', 'Accenture Express Tower');
+
+INSERT INTO
+    BUILDING (LOCATION_ID, ID, NAME)
+VALUES
+    ('BGLR', 'BGLR-B1', 'Accenture Pritech Park');
+
+INSERT INTO
+    BUILDING (LOCATION_ID, ID, NAME)
+VALUES
+    ('BGLR', 'BGLR-B3', 'Accenture Building 14');
+
+INSERT INTO
+    BUILDING (LOCATION_ID, ID, NAME)
+VALUES
+    ('GC', 'GC-B1', 'Accenture Grand Canal Plaza');
+
+INSERT INTO
+    ROOM (
+        FLOOR,
+        CAPACITY,
+        BUILDING_ID,
+        ID,
+        NAME,
+        DIRECTIONS,
+        IMAGES,
+        STATIONERY
+    )
+VALUES
+    (
+        '1',
+        '100',
+        'MUM-B1',
+        'MUM-B1-ID1',
+        'Meeting Room 1',
+        ARRAY ['Enter the building',
+        'Turn right',
+        'Walk straight',
+        'Turn left'],
+        ARRAY ['https://picsum.photos/1024/1024', 'https://picsum.photos/1024/1024', 'https://picsum.photos/1024/1024', 'https://picsum.photos/1024/1024'],
+        ARRAY ['whiteboard', 'projector']
+    );
+
+INSERT INTO
+    ROOM (
+        FLOOR,
+        CAPACITY,
+        BUILDING_ID,
+        ID,
+        NAME,
+        DIRECTIONS,
+        IMAGES,
+        STATIONERY
+    )
+VALUES
+    (
+        '1',
+        '100',
+        'MUM-B2',
+        'MUM-B2-ID1',
+        'Meeting Room 2',
+        ARRAY ['Enter the building',
+        'Turn left',
+        'Turn right'],
+        ARRAY ['https://picsum.photos/1024/1024', 'https://picsum.photos/1024/1024', 'https://picsum.photos/1024/1024', 'https://picsum.photos/1024/1024'],
+        ARRAY ['whiteboard']
+    );
+
+INSERT INTO
+    ROOM (
+        FLOOR,
+        CAPACITY,
+        BUILDING_ID,
+        ID,
+        NAME,
+        DIRECTIONS,
+        IMAGES,
+        STATIONERY
+    )
+VALUES
+    (
+        '15',
+        '100',
+        'GC-B1',
+        'GC-B1-ID1',
+        'Meeting Room 1',
+        ARRAY ['Enter the building',
+        'Turn left',
+        'Turn right'],
+        ARRAY ['https://picsum.photos/1024/1024', 'https://picsum.photos/1024/1024', 'https://picsum.photos/1024/1024', 'https://picsum.photos/1024/1024'],
+        ARRAY ['projector']
+    );
