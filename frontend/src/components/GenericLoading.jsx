f01cd674bd5d27cfff232a0aa56172eb881bf0a9
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2024-08-07 23:24:04 +0530

    ⚡ Reduce the size of the GenericLoading component

--- frontend/src/components/GenericLoading.jsx
+++ frontend/src/components/GenericLoading.jsx
@@ -1,18 +1,12 @@
 import React from "react";
-import Typography from "@mui/joy/Typography";
-import CircularProgress from "@mui/joy/CircularProgress";
 
 export default function GenericLoading({ text }) {
 	return (
 		<>
-			<Typography
-				startDecorator={<CircularProgress size="sm" variant="plain" />}
-				level="title-md"
-				sx={{ p: 1, m: 3 }}
-			>
-				{text}
+			<div style={{ padding: "20px" }}>
+				⏳ {text}
 				{!text && <>Loading. Please wait.</>}
-			</Typography>
+			</div>
 		</>
 	);
 }
